d8c38d14da9ea6f2ab10f2868b1b566928a70377
NickRodgers313 <nicholasrodgers342@gmail.com>
2025-02-07 16:12:03 -0500

    Initial Commit

---
+++ TheWord/TheWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWordApp.swift
+//  TheWord
+//
+//  Created by nick rodgers on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TheWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheWordTests/TheWordTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheWordTests.swift
+//  TheWordTests
+//
+//  Created by nick rodgers on 2/7/25.
+//
+
+import Testing
+@testable import TheWord
+
+struct TheWordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
